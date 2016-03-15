1f5579367d792f788683b347ab52b0c1abbb08f0
Dan Gaston <daniel.gaston@deaddriftbio.com>
2016-03-15 14:10:50 -0300

    Update amplicon workflow

--- workflow-somatic_amplicon.py
+++ workflow-somatic_amplicon.py
@@ -69,66 +69,85 @@ if __name__ == "__main__":
                                   memory="{}G".format(config['gatk']['max_mem']))
         # Variant Calling
         spawn_variant_job = Job.wrapJobFn(pipeline.spawn_variant_jobs)
-        freebayes_job = Job.wrapJobFn(freebayes.freebayes_single, config, sample, align_job.rv(),
+        freebayes_job = Job.wrapJobFn(freebayes.freebayes_single, config, sample, recal_job.rv(),
                                       cores=1,
                                       memory="{}G".format(config['freebayes']['max_mem']))
 
-        mutect_job = Job.wrapJobFn(mutect.mutect_single, config, sample, align_job.rv(),
+        mutect_job = Job.wrapJobFn(mutect.mutect_single, config, sample, recal_job.rv(),
                                    cores=1,
                                    memory="{}G".format(config['mutect']['max_mem']))
 
-        # mutect2_job = Job.wrapJobFn(mutect.mutect2_single, config, sample, align_job.rv(),
+        # mutect2_job = Job.wrapJobFn(mutect.mutect2_single, config, sample, recal_job.rv(),
         #                             cores=1,
         #                             memory="{}G".format(config['mutect']['max_mem']))
         #
-        vardict_job = Job.wrapJobFn(vardict.vardict_single, config, sample, samples, align_job.rv(),
+        vardict_job = Job.wrapJobFn(vardict.vardict_single, config, sample, samples, recal_job.rv(),
                                     cores=int(config['vardict']['num_cores']),
                                     memory="{}G".format(config['vardict']['max_mem']))
 
-        scalpel_job = Job.wrapJobFn(scalpel.scalpel_single, config, sample, samples, align_job.rv(),
+        scalpel_job = Job.wrapJobFn(scalpel.scalpel_single, config, sample, samples, recal_job.rv(),
                                     cores=int(config['scalpel']['num_cores']),
                                     memory="{}G".format(config['scalpel']['max_mem']))
 
-        # scanindel_job = Job.wrapJobFn(scanindel.scanindel, config, sample, samples, align_job.rv(),
-        #                               cores=int(config['scanindel']['num_cores']),
-        #                               memory="{}G".format(config['scanindel']['max_mem']))
-        #
-        # platypus_job = Job.wrapJobFn(platypus.platypus_single, config, sample, samples, align_job.rv(),
-        #                              cores=int(config['platypus']['num_cores']),
-        #                              memory="{}G".format(config['platypus']['max_mem']))
+        scanindel_job = Job.wrapJobFn(scanindel.scanindel, config, sample, samples, recal_job.rv(),
+                                      cores=int(config['scanindel']['num_cores']),
+                                      memory="{}G".format(config['scanindel']['max_mem']))
 
-        # pindel_job = Job.wrapJobFn(pindel.run_pindel, config, sample, align_job.rv(),
-        #                            cores=int(config['pindel']['num_cores']),
-        #                            memory="{}G".format(config['pindel']['max_mem']))
+        platypus_job = Job.wrapJobFn(platypus.platypus_single, config, sample, samples, recal_job.rv(),
+                                     cores=int(config['platypus']['num_cores']),
+                                     memory="{}G".format(config['platypus']['max_mem']))
+
+        pindel_job = Job.wrapJobFn(pindel.run_pindel, config, sample, recal_job.rv(),
+                                   cores=int(config['pindel']['num_cores']),
+                                   memory="{}G".format(config['pindel']['max_mem']))
 
         # Need to filter for on target only results somewhere as well
         spawn_normalization_job = Job.wrapJobFn(pipeline.spawn_variant_jobs)
-        normalization_job1 = Job.wrapJobFn(variation.vt_normalization, config, sample, "mutect",
-                                           mutect_job.rv(),
-                                           cores=1,
-                                           memory="{}G".format(config['gatk']['max_mem']))
 
-        normalization_job2 = Job.wrapJobFn(variation.vt_normalization, config, sample, "scalpel",
-                                           scalpel_job.rv(),
-                                           cores=1,
-                                           memory="{}G".format(config['gatk']['max_mem']))
-
-        normalization_job3 = Job.wrapJobFn(variation.vt_normalization, config, sample, "freebayes",
+        normalization_job1 = Job.wrapJobFn(variation.vt_normalization, config, sample, "freebayes",
                                            freebayes_job.rv(),
                                            cores=1,
                                            memory="{}G".format(config['gatk']['max_mem']))
 
-        normalization_job4 = Job.wrapJobFn(variation.vt_normalization, config, sample, "vardict",
+        normalization_job2 = Job.wrapJobFn(variation.vt_normalization, config, sample, "mutect",
+                                           mutect_job.rv(),
+                                           cores=1,
+                                           memory="{}G".format(config['gatk']['max_mem']))
+
+        normalization_job3 = Job.wrapJobFn(variation.vt_normalization, config, sample, "vardict",
                                            vardict_job.rv(),
                                            cores=1,
                                            memory="{}G".format(config['gatk']['max_mem']))
 
-        callers = "mutect,scalpel,freebayes,vardict"
+        normalization_job4 = Job.wrapJobFn(variation.vt_normalization, config, sample, "scalpel",
+                                           scalpel_job.rv(),
+                                           cores=1,
+                                           memory="{}G".format(config['gatk']['max_mem']))
+
+        normalization_job5 = Job.wrapJobFn(variation.vt_normalization, config, sample, "scanindel",
+                                           scanindel_job.rv(),
+                                           cores=1,
+                                           memory="{}G".format(config['gatk']['max_mem']))
+
+        normalization_job6 = Job.wrapJobFn(variation.vt_normalization, config, sample, "platypus",
+                                           platypus_job.rv(),
+                                           cores=1,
+                                           memory="{}G".format(config['gatk']['max_mem']))
+
+        normalization_job7 = Job.wrapJobFn(variation.vt_normalization, config, sample, "pindel",
+                                           pindel_job.rv(),
+                                           cores=1,
+                                           memory="{}G".format(config['gatk']['max_mem']))
+
+        callers = "freebayes, mutect, vardict, scalpel, scanindel, platypus, pindel"
 
         merge_job = Job.wrapJobFn(variation.merge_variant_calls, config, sample, callers, (normalization_job1.rv(),
                                                                                            normalization_job2.rv(),
                                                                                            normalization_job3.rv(),
-                                                                                           normalization_job4.rv()))
+                                                                                           normalization_job4.rv(),
+                                                                                           normalization_job5.rv(),
+                                                                                           normalization_job6.rv(),
+                                                                                           normalization_job7.rv()))
 
         # Removed temporarily until config generation script more easily adds in appropriate region files
         # on_target_job = Job.wrapJobFn(utilities.bcftools_filter_variants_regions, config, sample, samples,
