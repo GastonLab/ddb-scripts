f959aa0e08b0d8cbd97ac115eece92a901c12c4d
Dan Gaston <daniel.gaston@deaddriftbio.com>
2016-05-20 14:54:21 -0300

    Adding Salmon scripts

---
+++ workflow-RNA-Seq_Salmon.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+
+# Standard packages
+import sys
+import argparse
+
+# Third-party packages
+from toil.job import Job
+
+# Package methods
+from ddb import configuration
+from ddb_ngsflow import pipeline
+from ddb_ngsflow.rna import salmon
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-s', '--samples_file', help="Input configuration file for samples")
+    parser.add_argument('-c', '--configuration', help="Configuration file for various settings")
+    Job.Runner.addToilOptions(parser)
+    args = parser.parse_args()
+    args.logLevel = "INFO"
+
+    sys.stdout.write("Parsing configuration data\n")
+    config = configuration.configure_runtime(args.configuration)
+
+    sys.stdout.write("Parsing sample data\n")
+    samples = configuration.configure_samples(args.samples_file, config)
+
+    # Workflow Graph definition. The following workflow definition should create a valid Directed Acyclic Graph (DAG)
+    root_job = Job.wrapJobFn(pipeline.spawn_batch_jobs, cores=1)
+
+    # Per sample jobs
+    for sample in samples:
+        # Alignment and Refinement Stages
+        align_job = Job.wrapJobFn(salmon.salmon_paired, config, sample, samples,
+                                  cores=int(config['salmon']['num_cores']),
+                                  memory="{}G".format(config['salmon']['max_mem']))
+
+        # Create workflow from created jobs
+        root_job.addChild(align_job)
+
+
+    # Start workflow execution
+    Job.Runner.startToil(root_job, args)
